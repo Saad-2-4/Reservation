ad6592d6e275bff35d33dc5a6f25e3db2a850c24
Saad-2-4 <saadafkir10@gmail.com>
2024-10-27 14:04:26 +0100

    Add files via upload

---
+++ app/Rules/DateBetween.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class DateBetween implements ValidationRule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $pickupDate = Carbon::parse($value);
+        $lastDate = Carbon::now()->addWeek();
+
+        return $value >= now() && $value <= $lastDate;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Please choose the date between a week from  now.';
+    }
+}
---
+++ app/Rules/TimeBetween.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class TimeBetween implements ValidationRule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $pickupDate = Carbon::parse($value);
+        $pickupTime = Carbon::createFromTime($pickupDate->hour, $pickupDate->minute, $pickupDate->second);
+        // when the restaurant is open
+        $earliestTime = Carbon::createFromTimeString('17:00:00');
+        $lastTime = Carbon::createFromTimeString('23:00:00');
+
+        return $pickupTime->between($earliestTime, $lastTime) ? true : false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Please choose the time between 17:00-23:00.';
+    }
+}
